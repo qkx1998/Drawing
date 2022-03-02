38803923a50fe6d2ec012e9b02fba75aa280afa3
SlowDive <18438605721@163.com>
2022-03-02 14:05:51 +0800

    Update seaborn绘图风格.py

--- seaborn绘图风格.py
+++ seaborn绘图风格.py
@@ -1,1 +1,48 @@
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+import numpy as np
 
+a = np.arange(6)
+b = a ** 2
+
+# seaborn有5种绘图风格：darkgrid, whitegrid, dark, white, ticks
+# 设置风格的语句：
+sns.set_style('darkgrid')
+
+# 绘制出所有的风格
+styles = ['darkgrid', 'whitegrid', 'dark', 'white', 'ticks']
+for idx, style in enumerate(styles):
+    sns.set_style(style)
+    sns.barplot(a, b, )
+    plt.show()
+    
+# seaborn的调色板所有的配色：
+all_palettes = ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r',
+                'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r',
+                'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r',
+                'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r',
+                'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r',
+                'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r',
+                'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag',
+                'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 
+                'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r',
+                'hot', 'hot_r', 'hsv', 'hsv_r', 'icefire', 'icefire_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'mako', 'mako_r', 'nipy_spectral',
+                'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'rocket', 'rocket_r',
+                'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r',
+                'terrain', 'terrain_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'vlag', 'vlag_r', 'winter', 'winter_r']
+
+len(all_palettes) # 一共有172个
+
+# 绘制出所有的调色图样例图
+fig, axes = plt.subplots(29, 6, figsize=(20, 100))
+locs = [(x, y) for x in range(29) for y in range(6)]
+for i, p in enumerate(all_palettes):
+    # 加异常处理。因为画布有174个子图，我们只需要绘制172个，画不完会报错
+    try:
+        # 每个子图设置调色盘的名字
+        axes[locs[i]].set_title(p, fontsize=10)
+        # 在这里指定用哪个子图做出我们所需要的风格图！！
+        sns.barplot(a, b, palette=p, ax=axes[locs[i]])
+    except:
+        pass
